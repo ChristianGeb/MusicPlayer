f8c4f21b5dc011ba3211a41a049fc2b805978ffd
ChrisFennek <christian.gebhardt@online.de>
2021-01-07 16:16:10 +0100

    load songs

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+const image = document.querySelector("img");
+const title = document.querySelector("#title");
+const artist = document.querySelector("#artist");
 const music = document.querySelector("audio");
 const prevBtn = document.querySelector("#prev");
 const playBtn = document.querySelector("#play");
 const nextBtn = document.querySelector("#next");
+
+const songs = [{
+    name: "jacinto-1",
+    displayName: "Electric chill Machine",
+    artist: "Test"
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Remix",
+    artist: "Test2"
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Something",
+    artist: "Test3"
+  },
+  {
+    name: "jacinto-4",
+    displayName: "Electric Washing machiney",
+    artist: "Test4"
+  }
+];
 
 let isPlaying = false;
 
@@ -20,3 +45,12 @@ function pauseSong() {
 }
 
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+loadSong(songs[3]);
